2f519dcc42dd61e2178bf988d3e2099b6363dffc
yttkans <yttkans@gmail.com>
2022-07-18 03:08:01 +0900

    Improve quering

--- app/src/main/java/jp/mydns/lilium/booklist/repository/QueryRepository.kt
+++ app/src/main/java/jp/mydns/lilium/booklist/repository/QueryRepository.kt
@@ -6,9 +6,9 @@ class QueryRepository {
     companion object {
         private val space = Regex("\\s")
 
-        fun query(value: String?, items: List<Book>?): List<Book>? {
-            if (items == null) return null
-            if (value == null) return items
+        fun query(value: String?, items: List<Book>?): List<Book> {
+            if (items == null) return emptyList()
+            if (value == null) return emptyList()
 
             val vs = value.split(space).filter { it.isNotBlank() }
             return items.filter { item ->
